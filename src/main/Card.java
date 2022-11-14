dc1114c1cf813202af54cce34cd1a4cda75db084
Tarik <tarikomer00@gmail.com>
2022-11-14 04:16:48 +0200

    almost complete start game
    custom card class system added
    command parser - incomplete
    deck transformer: CardInput -> Custom class system
    added debugging commands code skeleton
    
    TODO: add player class, instantiate 2 player obj, finish command parser

--- src/main/Card.java
+++ src/main/Card.java
@@ -1,26 +1,53 @@
 package main;
 
+import fileio.CardInput;
+
 import java.util.ArrayList;
+import java.util.LinkedList;
 
-public interface Card {
-    // default coordinates - card is not on the table
-    int coordinateX = -1;
-    int coordinateY = -1;
-}
+public abstract class Card {
 
-abstract class MinionCard implements Card {
-    int mana = 0;
-    int health = 0;
-    int attackDamage = 0;
-    String description = null;
-    String colors = null;
-    String name = null;
+    private String description;
 
-    boolean isFrozen = false;
+    private ArrayList<String> colors;
 
-    boolean isTank = false;
+    private String name = null;
 
-    boolean ableToAttack = true;
+    private int mana = -1;
+
+    Card (String name, int mana, String description, ArrayList<String> colors) {
+        this.name = name;
+        this.colors = new ArrayList<>(colors);
+        this.mana = mana;
+        this.description = description;
+    }
+
+    public Card() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<String> getColors() {
+        return colors;
+    }
+
+    public void setColors(ArrayList<String> colors) {
+        this.colors = new ArrayList<>(colors);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public int getMana() {
         return mana;
@@ -28,6 +55,259 @@ abstract class MinionCard implements Card {
 
     public void setMana(int mana) {
         this.mana = mana;
+    }
+
+}
+
+class MinionCard extends Card {
+    private int health;
+
+    private int attackDamage;
+
+    private boolean isFrozen = false;
+
+    private boolean ableToAttack = true;
+
+    private boolean isTank = false;
+
+    public MinionCard() {
+
+    }
+
+    public MinionCard(CardInput rawCard) {
+        super(rawCard.getName(), rawCard.getMana(), rawCard.getDescription(), rawCard.getColors());
+        this.health = rawCard.getHealth();
+        this.attackDamage = rawCard.getAttackDamage();
+        // when needed, consider minion tank
+        if (this.getName().equals("Warden") || this.getName().equals("Goliath")) {
+            isTank = true;
+        }
+    }
+
+    public boolean isDead() {
+        if (this.health <= 0)
+            return true;
+        else
+            return false;
+    }
+
+    public boolean isTank() {
+        return isTank;
+    }
+
+    public void setTank(boolean tank) {
+        isTank = tank;
+    }
+
+    public int getAttackDamage() {
+        return attackDamage;
+    }
+
+    public void setAttackDamage(int attackDamage) {
+        this.attackDamage = attackDamage;
+    }
+
+    public void attackMinion(MinionCard attackedCard) {
+        attackedCard.health = attackedCard.health - this.getAttackDamage();
+        this.ableToAttack = false;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public boolean isFrozen() {
+        return isFrozen;
+    }
+
+    public void setFrozen(boolean frozen) {
+        isFrozen = frozen;
+    }
+
+    public boolean isAbleToAttack() {
+        return ableToAttack;
+    }
+
+    public void setAbleToAttack(boolean ableToAttack) {
+        this.ableToAttack = ableToAttack;
+    }
+
+    @Override
+    public String toString() {
+        return "{"
+                +  "mana="
+                + this.getMana()
+                +  ", attackDamage="
+                + attackDamage
+                + ", health="
+                + health
+                +  ", description='"
+                + this.getDescription()
+                + '\''
+                + ", colors="
+                + this.getColors()
+                + ", name='"
+                +  ""
+                + this.getName()
+                + '\''
+                + '}';
+    }
+
+}
+
+class Ripper extends MinionCard {
+    public Ripper(CardInput rawCard) {
+        super(rawCard);
+    }
+
+    void weakKneesAbility() {
+
+    }
+
+}
+
+class Miraj extends MinionCard {
+    public Miraj(CardInput rawCard) {
+        super(rawCard);
+    }
+    void skyjackAbility() {
+
+    }
+}
+
+class CursedOne extends MinionCard {
+    public CursedOne(CardInput rawCard) {
+        super(rawCard);
+    }
+    void shapeshiftAbility() {
+
+    }
+}
+class Disciple extends MinionCard {
+    public Disciple(CardInput rawCard) {
+        super(rawCard);
+    }
+    void godsPlanAbility() {
+
+    }
+}
+
+class FirestormEnvironmentCard extends Card {
+    public FirestormEnvironmentCard(CardInput rawCard) {
+        super(rawCard.getName(), rawCard.getMana(), rawCard.getDescription(), rawCard.getColors());
+    }
+
+    void firestormEffect(LinkedList<MinionCard> cards) {
+        // deal damage to all cards
+        for (MinionCard card : cards) {
+            card.setHealth(card.getHealth() - 1);
+        }
+    }
+    @Override
+    public String toString() {
+        return "{"
+                +  "mana="
+                + this.getMana()
+                +  ", description='"
+                + this.getDescription()
+                + '\''
+                + ", colors="
+                + this.getColors()
+                + ", name='"
+                +  ""
+                + this.getName()
+                + '\''
+                + '}';
+    }
+}
+
+class WinterfellEnvironmentCard extends Card {
+    public WinterfellEnvironmentCard(CardInput rawCard) {
+        super(rawCard.getName(), rawCard.getMana(), rawCard.getDescription(), rawCard.getColors());
+    }
+
+    void winterfellEffect(LinkedList<MinionCard> cards) {
+        // take ability to attack (will be reset next round)
+        for (MinionCard card : cards) {
+            card.setFrozen(true);
+            card.setAbleToAttack(false);
+        }
+    }
+    @Override
+    public String toString() {
+        return "{"
+                +  "mana="
+                + this.getMana()
+                +  ", description='"
+                + this.getDescription()
+                + '\''
+                + ", colors="
+                + this.getColors()
+                + ", name='"
+                +  ""
+                + this.getName()
+                + '\''
+                + '}';
+    }
+}
+
+class HeartHoundEnvironmentCard extends Card {
+    public HeartHoundEnvironmentCard(CardInput rawCard) {
+        super(rawCard.getName(), rawCard.getMana(), rawCard.getDescription(), rawCard.getColors());
+    }
+
+    MinionCard heartHoundEffect(LinkedList<MinionCard> cards) {
+        // initially null; if row is empty, will return null
+        MinionCard maxHealthMinionCard = null;
+
+        if (cards.size() == 1) {
+            // if only one card, it is max hp card
+            maxHealthMinionCard = cards.getFirst();
+        } else if (cards.size() > 1) {
+            // if multiple, get max hp card
+            maxHealthMinionCard = cards.getFirst();
+
+            for (int i = 1; i < cards.size(); i++)
+                if (cards.get(i).getHealth() > maxHealthMinionCard.getHealth())
+                    maxHealthMinionCard = cards.get(i);
+        }
+
+        return maxHealthMinionCard;
+    }
+
+    @Override
+    public String toString() {
+        return "{"
+                +  "mana="
+                + this.getMana()
+                +  ", description='"
+                + this.getDescription()
+                + '\''
+                + ", colors="
+                + this.getColors()
+                + ", name='"
+                +  ""
+                + this.getName()
+                + '\''
+                + '}';
+    }
+}
+
+class HeroCard extends Card {
+    private int health = 30;
+
+    private int attackDamage;
+
+    public HeroCard() {
+
+    }
+    public HeroCard(CardInput rawCard) {
+        super(rawCard.getName(), rawCard.getMana(), rawCard.getDescription(), rawCard.getColors());
+        this.attackDamage = rawCard.getAttackDamage();
     }
 
     public int getHealth() {
@@ -46,102 +326,64 @@ abstract class MinionCard implements Card {
         this.attackDamage = attackDamage;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getColors() {
-        return colors;
-    }
-
-    public void setColors(String colors) {
-        this.colors = colors;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public boolean isFrozen() {
-        return isFrozen;
-    }
-
-    public void setFrozen(boolean frozen) {
-        isFrozen = frozen;
-    }
-
-    public boolean isTank() {
-        return isTank;
-    }
-
-    public void setTank(boolean tank) {
-        isTank = tank;
-    }
-
-    public boolean isAbleToAttack() {
-        return ableToAttack;
-    }
-
-    public void setAbleToAttack(boolean ableToAttack) {
-        this.ableToAttack = ableToAttack;
-    }
-
-    public boolean isDead() {
-        if (this.getHealth() <= 0)
-            return true;
-        else
-            return false;
-    }
-
-    public void attackMinion(MinionCard attackedCard) {
-        attackedCard.setHealth(attackedCard.getHealth() - this.getAttackDamage());
-        this.setAbleToAttack(false);
+    @Override
+    public String toString() {
+        return "{"
+                +  "mana="
+                + this.getMana()
+                +  ", attackDamage="
+                + attackDamage
+                + ", health="
+                + health
+                +  ", description='"
+                + this.getDescription()
+                + '\''
+                + ", colors="
+                + this.getColors()
+                + ", name='"
+                +  ""
+                + this.getName()
+                + '\''
+                + '}';
     }
 }
 
-abstract class StandardMinionCard extends MinionCard {
+class LordRoyce extends HeroCard {
+    public LordRoyce(CardInput rawCard) {
+        super(rawCard);
+    }
 
+    void subZeroAbility() {
+
+    }
 }
 
-class Sentinel extends StandardMinionCard {
+class EmpressThorina extends HeroCard {
+    public EmpressThorina(CardInput rawCard) {
+        super(rawCard);
+    }
+
+    void lowBlowAbility() {
+
+    }
 }
 
-class Berserker extends StandardMinionCard {
+class KingMudface extends HeroCard {
+    public KingMudface(CardInput rawCard) {
+        super(rawCard);
+    }
 
+    void earthBornAbility() {
+
+    }
 }
 
-class Goliath extends StandardMinionCard {
+class GeneralKocioraw extends HeroCard {
+    public GeneralKocioraw(CardInput rawCard) {
+        super(rawCard);
+    }
 
-}
+    void bloodThirstAbility() {
 
-class Warden extends StandardMinionCard {
-
-}
-
-abstract class SpecialMinionCard extends MinionCard {
-
-}
-
-class Ripper extends SpecialMinionCard {
-
-}
-
-class Miraj extends SpecialMinionCard {
-
-}
-
-class CursedOne extends SpecialMinionCard {
-
-}
-
-class Disciple extends SpecialMinionCard {
-
+    }
 }
--- src/main/RunningGame.java
+++ src/main/RunningGame.java
@@ -3,44 +3,140 @@ package main;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import fileio.*;
 
+import java.lang.reflect.AnnotatedArrayType;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Random;
 
 public class RunningGame {
     ArrayNode output;
     ArrayList<GameInput> games;
 
-    ArrayList<DecksInput> playerOneDecks;
+    DecksInput playerOneDecks;
 
-    ArrayList<DecksInput> getPlayerTwoDecks;
-
-    CardInput[][] gameTable = new CardInput[4][5];
-
-    Player playerOne;
-    Player playerTwo;
+    DecksInput playerTwoDecks;
 
     public RunningGame() {
 
     }
 
     public RunningGame(Input inputData, ArrayNode output) {
-        // constructor that copies games and player data
+        // link to output
         this.output = output;
-        games = new ArrayList<>(inputData.getGames());
-        playerOne = new Player(inputData.getPlayerOneDecks());
-        playerTwo = new Player(inputData.getPlayerTwoDecks());
+
+        // list of games, with its corresponding information
+        this.games = new ArrayList<>(inputData.getGames());
+
+        // objects that hold decks for player 1 and 2 (and relevant information)
+        this.playerOneDecks = inputData.getPlayerOneDecks();
+        this.playerTwoDecks = inputData.getPlayerTwoDecks();
     }
 
     public void runGame() {
+
         for (GameInput game : games) {
-            StartGameInput startGameInput = game.getStartGame();
+            // initialize empty table
+            ArrayList<LinkedList<MinionCard>> table = new ArrayList<>(4);
+
             ArrayList<ActionsInput> actionsInput = game.getActions();
+
+            // TODO: place this into a player class, instantiate 2 player obj
+//            ArrayList<Card> playerOneHand = new ArrayList<>();
+//            ArrayList<Card> playerTwoHand = new ArrayList<>();
+//
+//            // get current game decks for each player
+//            ArrayList<Card> playerOneDeck = RunningGame.processDeck
+//                    (playerOneDecks.getDecks().get(game.getStartGame().getPlayerOneDeckIdx()),
+//                            game.getStartGame().getShuffleSeed());
+//            ArrayList<Card> playerTwoDeck = RunningGame.processDeck
+//                    (playerTwoDecks.getDecks().get(game.getStartGame().getPlayerOneDeckIdx()),
+//                            game.getStartGame().getShuffleSeed());
+//
+//            int playerOneMana = 0;
+//            int playerTwoMana = 0;
+
+            for (ActionsInput command : actionsInput) {
+                // process each command
+            }
         }
+    }
+
+    public static ArrayList<Card> processDeck(ArrayList<CardInput> rawDeck, int shuffleSeed) {
+        ArrayList<Card> processedDeck = new ArrayList<>();
+
+        for (CardInput rawCard : rawDeck) {
+            Card processedCard;
+
+            // if card has certain name, make it object of said class
+            if (rawCard.getName().equals("Goliath") || rawCard.getName().equals("Warden") ||
+                    rawCard.getName().equals("Sentinel") || rawCard.getName().equals("Berserker")) {
+                processedCard = new MinionCard(rawCard);
+                processedDeck.add(processedCard);
+            } else if (rawCard.getName().equals("The Ripper")) {
+                processedCard = new Ripper(rawCard);
+            } else if (rawCard.getName().equals("Miraj")) {
+                processedCard = new Miraj(rawCard);
+            } else if (rawCard.getName().equals("The Cursed One")) {
+                processedCard = new CursedOne(rawCard);
+            } else if (rawCard.getName().equals("Disciple")) {
+                processedCard = new Disciple(rawCard);
+            } else if (rawCard.getName().equals("Firestorm")) {
+                processedCard = new FirestormEnvironmentCard(rawCard);
+            } else if (rawCard.getName().equals("Winterfell")) {
+                processedCard = new WinterfellEnvironmentCard(rawCard);
+            } else if (rawCard.getName().equals("Heart Hound")) {
+                processedCard = new HeartHoundEnvironmentCard(rawCard);
+            } else {
+                System.out.println("Wrong hero, bro");
+            }
+        }
+
+        // shuffle the chosen deck
+        Random random = new Random(shuffleSeed);
+        Collections.shuffle(processedDeck, random);
+
+        return processedDeck;
     }
 
 }
 
 class DebuggingCommands {
+    public static void getCardsInHand() {
 
+    }
+
+    public static void getPlayerDeck() {
+
+    }
+
+    public static void getCardsOnTable() {
+
+    }
+
+    public static void getPlayerTurn() {
+
+    }
+
+    public static void getPlayerHero() {
+
+    }
+
+    public static void getCardAtPosition() {
+
+    }
+
+    public static void getPlayerMana() {
+
+    }
+
+    public static void getEnvironmentCardsInHand() {
+
+    }
+
+    public static void getFrozenCardsOnTable() {
+
+    }
 }
 
 class Commands {
